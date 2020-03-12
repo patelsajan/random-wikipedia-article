3991d889d708f49b2221bd784c6b1ad35b5e796c
sajan patel <chinmayapatel7@gmail.com>
2020-03-13 00:35:44 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'random';
+
+
+  constructor( private http: HttpClient) {
+  }
+
+  error = false;
+
+  url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json&origin=* ';
+  cdata: any;
+  contentUrl = 'https://en.wikipedia.org/w/api.php?action=parse&prop=text&formatversion=2&origin=*&format=json&pageid=';
+  content: any;
+
+
+  getRandom(): void {
+    this.http.get(this.url).subscribe(data => {
+        console.log('random:', data);
+        this.cdata = data;
+        // this.contentUrl += this.cdata.query.random[0].id;
+        console.log('contentUrl:', this.contentUrl);
+        this.http.get(this.contentUrl + this.cdata.query.random[0].id).subscribe(content => {
+            console.log('content:', content);
+            this.content = content;
+        }
+        , error => { console.log(error);
+                     this.error = true;
+                     this.content = null; });
+    });
+  }
+
 }
